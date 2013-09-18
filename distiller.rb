91bf6548884f3f2291ee9a55c07c113f00bb2045
John Berry <ulfmagnetics@gmail.com>
2013-09-17 22:46:38 -0700

    Added simple command-line interface.

--- distiller.rb
+++ distiller.rb
@@ -2,6 +2,10 @@ require 'bundler'
 require 'csv'
 
 class Distiller
+  def self.usage
+    puts "Usage: #{$0} <midi csv> <output csv>"
+  end
+
   def initialize(csv)
     @raw_rows = CSV.parse(csv)
   end
@@ -26,5 +30,20 @@ class Distiller
 
   def note_number_with_offset(num, offset)
     num - offset
+  end
+end
+
+if ARGV.size != 2
+  Distiller.usage
+  exit
+end
+
+midi_csv = File.read(ARGV[0])
+CSV.open(ARGV[1], "wb") do |csv|
+  options = {}
+  options.merge!(root_note: ENV['ROOT_NOTE'].to_i) if ENV['ROOT_NOTE']
+  options.merge!(ticks_per_unit: ENV['TICKS_PER_UNIT'].to_i) if ENV['TICKS_PER_UNIT']
+  Distiller.new(midi_csv).rows(options).map do |row| 
+    csv << row
   end
 end
